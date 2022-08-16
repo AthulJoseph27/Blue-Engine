2003822b030bb8465a4af53a3f9519d1f168bc98
Athul Joseph <athuljoseph27052001@gmail.com>
2022-08-16 08:36:43 +0530

    Initial Commit

---
+++ Blue2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Blue2
+//
+//  Created by Athul Joseph on 16/08/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Blue2UITests/Blue2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Blue2UITestsLaunchTests.swift
+//  Blue2UITests
+//
+//  Created by Athul Joseph on 16/08/22.
+//
+
+import XCTest
+
+class Blue2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
